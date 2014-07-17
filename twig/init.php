d5a49636d1cd2fa45fbbf93954f69bde2f1bf3d3
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-07-17 15:17:27 +0200

    updated Twig to 1.16

--- twig/init.php
+++ twig/init.php
@@ -1,14 +1,9 @@
 <?php
-require_once __DIR__ . '/libs/Twig/Autoloader.php';
-Twig_Autoloader::register();
+require_once __DIR__ . '/vendor/autoload.php';
 
 function renderTemplate($file, array $params = array())
 {
-	static $twig;
-	if ($twig === NULL) {
-		$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
-		$twig = new Twig_Environment($loader, array('cache' => __DIR__ . '/cache'));
-	}
-
+	$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+	$twig = new Twig_Environment($loader, array('cache' => __DIR__ . '/cache'));
 	echo $twig->render($file . '.html', $params);
 }
