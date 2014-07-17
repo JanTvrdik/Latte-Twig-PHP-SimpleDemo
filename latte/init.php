667438806ae5246d398570d9fdc76f13497ea4d8
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-07-17 15:16:51 +0200

    updated Latte to 2.2.2

--- latte/init.php
+++ latte/init.php
@@ -1,28 +1,9 @@
 <?php
-require __DIR__ . '/libs/Nette/common/exceptions.php';
-require __DIR__ . '/libs/Nette/common/Object.php';
-require __DIR__ . '/libs/Nette/Loaders/NetteLoader.php';
-
-Nette\Loaders\NetteLoader::getInstance()->register();
-
-// uncomment to enable better error visualization
-// Nette\Diagnostics\Debugger::enable();
-// Nette\Diagnostics\Debugger::$strictMode = TRUE;
+require_once __DIR__ . '/vendor/autoload.php';
 
 function renderTemplate($file, array $params = array())
 {
-	static $cacheStorage;
-	if ($cacheStorage === NULL) $cacheStorage = new Nette\Caching\Storages\PhpFileStorage(__DIR__ . '/cache');
-
-	$template = new Nette\Templating\FileTemplate(__DIR__ . '/templates/' . $file . '.latte');
-	$template->setCacheStorage($cacheStorage);
-	$template->registerHelperLoader('Nette\Templating\Helpers::loader');
-	$template->onPrepareFilters[] = function ($template) {
-		static $latte;
-		if ($latte === NULL) $latte = new Nette\Latte\Engine();
-		$template->registerFilter($latte);
-	};
-
-	$template->setParameters($params);
-	$template->render();
+	$latte = new Latte\Engine();
+	$latte->setTempDirectory(__DIR__ . '/cache');
+	$latte->render(__DIR__ . '/templates/' . $file . '.latte', $params);
 }
